cdba07da8b116cf6c56ee7f869799f74575c8dbe
zhhrozhh <zhangh40@msu.edu>
2016-11-10 21:27:57 -0500

    bridge

--- X0/bridge.py
+++ X0/bridge.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from sys import argv
+from config0 import *
 if __name__ == '__main__':
 	sp = Image.open(argv[1])
 	ep = Image.open(argv[2])
@@ -11,6 +12,16 @@ if __name__ == '__main__':
 		for x in range(sp.size[0]):
 			for y in range(sp.size[1]):
 				if x%fs == i or y%fs == i:
+					img.putpixel((x,y),(0,0,0))
+					s[x,y] = (0,0,0)
+				else:
+					img.putpixel((x,y),s[x,y])
+		img.save(xdir+'t'+str(i)+'.png')
+	for i in range(fs):
+		img = Image.new('L',sp.size)
+		for x in range(sp.size[0]):
+			for y in range(sp.size[1]):
+				if x%fs == i or y%fs == i:
 					img.putpixel((x,y),e[x,y])
-		img.save('/Users/zhangh40/Downloads/golly-2.8-mac/X0/t'+str(i)+'.png')
+		img.save(xdir+'m'+str(i)+'.png')
 
